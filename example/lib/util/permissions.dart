4537487d4261f0a3fd9775d592620b48a64b630e
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-10-30 13:13:44 +0200

    #15: Fixed the example for android 12

--- example/lib/util/permissions.dart
+++ example/lib/util/permissions.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<bool> checkPermission() async {
   if (Platform.isIOS) return true;
+  final androidInfo = await DeviceInfoPlugin().androidInfo;
+  if ((androidInfo.version.sdkInt ?? 0) >= 31) {
+    var status = await Permission.bluetoothScan.status;
+    if (status.isDenied) {
+      status = await Permission.bluetoothScan.request();
+    }
+    return status.isGranted;
+  }
   var status = await Permission.location.status;
   if (status.isDenied) {
     status = await Permission.location.request();
